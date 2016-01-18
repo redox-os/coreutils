a6cba38bb1c702fc603f445ecba4454566f4565b
Polymetric <polymetricofficial@gmail.com>
2016-01-17 23:40:16 -0500

    implement touch

--- src/bin/touch.rs
+++ src/bin/touch.rs
@@ -1,3 +1,16 @@
-fn main() {
+use std::env;
+use std::fs::File;
+use std::process;
 
+fn main() {
+    if env::args().count() < 2 {
+        println!("No arguments provided!");
+        process::exit(1);
+    }
+
+    for arg in env::args().skip(1) {
+        if let Err(_) = File::open(arg.to_string()) {
+            File::create(arg).unwrap();
+        }
+    }
 }
