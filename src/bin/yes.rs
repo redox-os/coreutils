211c94283ada2f3e009c75cb9c37d064a8e3e055
Jeremy Soller <jackpot51@gmail.com>
2016-02-18 11:45:45 -0700

    Fix yes on Redox

--- src/bin/yes.rs
+++ src/bin/yes.rs
@@ -2,6 +2,7 @@ extern crate coreutils;
 
 use std::env;
 use std::io::{Write, stdout};
+use std::thread;
 
 use coreutils::extra::{OptionalExt};
 
@@ -9,14 +10,17 @@ fn main() {
     let mut stdout = stdout();
 
     let answer = env::args().skip(1).next();
-    if let Some(x) = answer {
+    if let Some(mut x) = answer {
+        x.push('\n');
         let print = x.as_bytes();
         loop {
             stdout.write(print).try();
+            thread::yield_now();
         }
     } else {
         loop {
-            stdout.write(b"y").try();
+            stdout.write(b"y\n").try();
+            thread::yield_now();
         }
     }; // Dafuq, borrowck
 }
