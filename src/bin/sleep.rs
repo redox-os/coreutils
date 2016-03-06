7c638da0aca14cd157a949da2c51bd4b0734fd0e
Michael Murphy <mmstickman@gmail.com>
2016-03-06 12:14:11 -0500

    Fix Issues
    - Convert `static` variables to `const`
    - Remove command-name prefixes from error strings
    - Print errors to `stderr` instead of `stdout
    - Don't use the `println!` macro
    - Remove Jeremy Soller's name

--- src/bin/sleep.rs
+++ src/bin/sleep.rs
@@ -10,29 +10,31 @@ use std::time::Duration;
 
 use coreutils::extra::{OptionalExt};
 
-static MAN_PAGE: &'static str = r#"NAME
+const MAN_PAGE: &'static str = r#"NAME
     sleep - delay for a specified amount of time
 
 SYNOPSIS
-    sleep NUMBER[SUFFIX]...
-    sleep OPTION
+    sleep [-h | --help] NUMBER[SUFFIX]...
 
 DESCRIPTION
     Pause the shell for NUMBER seconds. An optional SUFFIX may be applied, such as 's' for seconds (default), 'm' for minutes, 'h' for hours or 'd' for day. Given multiple arguments, it will pause for the amount of time specified by the sum of their values. This implementation supports floating point numbers as input.
 
-    Example: sleep 60; sleep 1m; sleep 30s 30s;
-             sleep 0.5; sleep 0.5s; sleep 0.25s 0.25s
+EXAMPLE
+    The following are three possible arguments with the same effect:
+
+    sleep {90, 1.5m, 1m 30s}
 
 OPTIONS
-    -h, --help
+    -h
+    --help
         display this help and exit
 
 AUTHOR
-    Written by Jeremy Soller and Michael Murphy.
+    Written by Michael Murphy.
 "#;
 
-static MISSING_OPERAND: &'static str       = "sleep: missing operand";
-static HELP_INFO: &'static str             = "Try 'sleep --help' for more information.";
+const MISSING_OPERAND: &'static str = "missing operand";
+const HELP_INFO:       &'static str = "Try 'sleep --help' for more information.";
 
 fn main() {
     let stdout     = io::stdout();
@@ -46,7 +48,6 @@ fn main() {
                 stdout.write(MAN_PAGE.as_bytes()).try(&mut stderr);
             },
             _ => {
-                // TODO: (not supported in Redox due to missing _mulodi4) thread::sleep(Duration::new(arg.parse().try(), 0))
                 thread::sleep(Duration::from_millis(argument_to_ms(&arg)));
                 for argument in args {
                     thread::sleep(Duration::from_millis(argument_to_ms(&argument)));
@@ -54,7 +55,7 @@ fn main() {
             }
         }
     } else {
-        stdout.write(MISSING_OPERAND.as_bytes()).try(&mut stderr);
+        stderr.write(MISSING_OPERAND.as_bytes()).try(&mut stderr);
         stdout.write(HELP_INFO.as_bytes()).try(&mut stderr);
     }
 }
@@ -65,6 +66,8 @@ fn argument_to_ms(argument: &str) -> u64 {
     if let Ok(number) = argument.parse::<f64>() {
         return (number * 1000f64) as u64;
     }
+
+    let mut stderr = io::stderr();
 
     // Split the argument into two strings at the last character. The first string should be the
     // number while the second string should be the duration unit:
@@ -77,12 +80,18 @@ fn argument_to_ms(argument: &str) -> u64 {
             "h" => (number * 3600000f64) as u64,
             "d" => (number * 86400000f64) as u64,
             _   => {
-                println!("sleep: invalid time interval '{}'", argument);
+                let mut output = String::from("invalid time interval '");
+                output.push_str(argument);
+                output.push_str("\'\n");
+                stderr.write(output.as_bytes()).try(&mut stderr);
                 std::process::exit(1);
             }
         }
     } else {
-        println!("sleep: invalid time interval '{}'", argument);
+        let mut output = String::from("invalid time interval '");
+        output.push_str(argument);
+        output.push_str("\'\n");
+        stderr.write(output.as_bytes()).try(&mut stderr);
         std::process::exit(1);
     }
 }
