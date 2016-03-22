b78188aa2ca020190dd50349161e85dda7ff1272
Ticki <Ticki@users.noreply.github.com>
2016-03-22 20:24:59 +0100

    Use fs::rename instead

--- src/bin/mv.rs
+++ src/bin/mv.rs
@@ -4,7 +4,7 @@ extern crate extra;
 
 use std::env;
 use std::fs;
-use std::io::{self, stderr};
+use std::io::stderr;
 use extra::option::OptionalExt;
 
 fn main() {
@@ -12,11 +12,5 @@ fn main() {
     let ref src = env::args().nth(1).fail("no source argument.", &mut stderr);
     let ref dst = env::args().nth(2).fail("no destination argument.", &mut stderr);
 
-    let mut src_file = fs::File::open(src).try(&mut stderr);
-    let mut dst_file = fs::File::create(dst).try(&mut stderr);
-
-    io::copy(&mut src_file, &mut dst_file).try(&mut stderr);
-    drop(src_file); // close file
-
-    fs::remove_file(src).try(&mut stderr);
+    fs::rename(src, dst).try(&mut stderr);
 }
