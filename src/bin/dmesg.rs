881664ce259389862bae181999e19636773dbe7e
Jeremy Soller <jackpot51@gmail.com>
2016-04-26 22:04:18 -0600

    add dmesg

---
+++ src/bin/dmesg.rs
@@ -0,0 +1,28 @@
+//#![deny(warnings)]
+
+extern crate extra;
+
+use std::fs::File;
+use std::io;
+
+use extra::option::OptionalExt;
+
+const MAN_PAGE: &'static str = /* @MANSTART{ps} */ r#"
+NAME
+    dmesg - display the system message buffer
+
+SYNOPSIS
+    dmesg
+
+DESCRIPTION
+    Displays the contents of the system message buffer.
+"#; /* @MANEND */
+
+fn main() {
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+    let mut stderr = io::stderr();
+
+    let mut file = File::open("syslog:").try(&mut stderr);
+    io::copy(&mut file, &mut stdout).try(&mut stderr);
+}
