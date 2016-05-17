096642308683a6340a48b63c4c17d27fcd162834
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2016-05-17 23:51:48 +0300

    Implement man page for mkdir, print it when executing mkdir --help.

--- src/bin/mkdir.rs
+++ src/bin/mkdir.rs
@@ -4,15 +4,38 @@ extern crate extra;
 
 use std::env;
 use std::fs;
-use std::io;
+use std::io::{stdout, stderr, Write};
 use extra::io::fail;
 use extra::option::OptionalExt;
 
+const MAN_PAGE: &'static str = /* @MANSTART{mkdir} */ r#"
+NAME
+    mkdir - make directories
+
+SYNOPSIS
+    mkdir DIRECTORIES...
+
+DESCRIPTION
+    The mkdir utility creates the directories named as operands.
+"#; /* @MANEND */
+
 fn main() {
-    let mut stderr = io::stderr();
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    let mut stderr = stderr();
 
     if env::args().count() < 2 {
-        fail("no arguments.", &mut stderr);
+        fail("No arguments. Use --help to see the usage.", &mut stderr);
+    }
+
+    if env::args().count() == 2 {
+        if let Some(arg) = env::args().last() {
+            if arg == "--help" {
+                stdout.write_all(MAN_PAGE.as_bytes()).try(&mut stderr);
+                stdout.flush().try(&mut stderr);
+                return;
+            }
+        }
     }
 
     for ref path in env::args().skip(1) {
