930ac5d85b5d9c0fde3098a65893a938b2539a07
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2016-05-19 23:18:32 +0300

    Add manual page to yes, -h and --help options.

--- src/bin/yes.rs
+++ src/bin/yes.rs
@@ -3,14 +3,41 @@
 extern crate extra;
 
 use std::env;
-use std::io::{stdout, stderr};
+use std::io::{stdout, stderr, Write};
 use extra::io::WriteExt;
 use extra::option::OptionalExt;
+
+const MAN_PAGE: &'static str = /* @MANSTART{yes} */ r#"
+NAME
+    yes - print the letter y or a given word, forever.
+
+SYNOPSIS
+    seq [ [ -h | --help ] | [ word ] ]
+
+DESCRIPTION
+    The yes utility prints the word passed as an operand, forever. If no operand is passed, then
+    it defaults to the letter 'y'.
+
+OPTIONS
+    -h
+    --help
+        Print this manual page.
+"#; /* @MANEND */
 
 fn main() {
     let stdout = stdout();
     let mut stdout = stdout.lock();
     let mut stderr = stderr();
+
+    if env::args().count() == 2 {
+        if let Some(arg) = env::args().nth(1) {
+            if arg == "--help" || arg == "-h" {
+                stdout.write_all(MAN_PAGE.as_bytes()).try(&mut stderr);
+                stdout.flush().try(&mut stderr);
+                return;
+            }
+        }
+    }
 
     let answer = env::args().skip(1).next();
     if let Some(x) = answer {
