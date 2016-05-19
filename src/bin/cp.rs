5c2f2285aa5e0a0571bda011a4281c448d60a320
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2016-05-19 21:14:09 +0300

    Add manual page to cp, -h and --help options

--- src/bin/cp.rs
+++ src/bin/cp.rs
@@ -4,17 +4,46 @@ extern crate extra;
 
 use std::env;
 use std::fs;
-use std::io::{self, Seek, SeekFrom, stderr};
+use std::io::{self, Seek, SeekFrom, stderr, stdout, Write};
 use extra::io::fail;
 use extra::option::OptionalExt;
 
+const MAN_PAGE: &'static str = /* @MANSTART{cp} */ r#"
+NAME
+    cp - copy files
+
+SYNOPSIS
+    cp SOURCE_FILE DESTINATION_FILES...
+
+DESCRIPTION
+    The cp utility copies the contents of the SOURCE_FILE to the DESTINATION_FILES. If multiple
+    destionation files are specified, then multiple copies of SOURCE_FILE are created.
+
+OPTIONS
+    --help, -h
+        print this message
+"#; /* @MANEND */
+
 fn main() {
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
     let mut stderr = stderr();
-    let ref src = env::args().nth(1).fail("no source argument.", &mut stderr);
+
+    if env::args().count() == 2 {
+        if let Some(arg) = env::args().nth(1) {
+            if arg == "--help" || arg == "-h" {
+                stdout.write_all(MAN_PAGE.as_bytes()).try(&mut stderr);
+                stdout.flush().try(&mut stderr);
+                return;
+            }
+        }
+    }
+
+    let ref src = env::args().nth(1).fail("No source argument. Use --help to see the usage.", &mut stderr);
     let dsts = env::args().skip(2);
 
     if dsts.len() < 1 {
-        fail("no destination arguments.", &mut stderr);
+        fail("No destination arguments. Use --help to see the usage.", &mut stderr);
     }
 
     let mut src_file = fs::File::open(src).try(&mut stderr);
