66119f649a597f425fd6bdc25b22d074e9a56bc3
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2016-05-19 22:32:11 +0300

    Add man page to rm, -h and --help options.

--- src/bin/rm.rs
+++ src/bin/rm.rs
@@ -4,15 +4,42 @@ extern crate extra;
 
 use std::env;
 use std::fs;
-use std::io;
+use std::io::{stdout, stderr, Write};
 use extra::io::fail;
 use extra::option::OptionalExt;
 
+const MAN_PAGE: &'static str = /* @MANSTART{rm} */ r#"
+NAME
+    rm - delete files
+
+SYNOPSIS
+    rm [ -h | --help ] FILE...
+
+DESCRIPTION
+    The rm utility deletes the file named by the FILE operand. Multiple file names can be passed.
+
+OPTIONS
+    --help, -h
+        print this message
+"#; /* @MANEND */
+
 fn main() {
-    let mut stderr = io::stderr();
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    let mut stderr = stderr();
+
+    if env::args().count() == 2 {
+        if let Some(arg) = env::args().nth(1) {
+            if arg == "--help" || arg == "-h" {
+                stdout.write_all(MAN_PAGE.as_bytes()).try(&mut stderr);
+                stdout.flush().try(&mut stderr);
+                return;
+            }
+        }
+    }
 
     if env::args().count() < 2 {
-        fail("no arguments.", &mut stderr);
+        fail("No arguments. Use --help to see the usage.", &mut stderr);
     }
 
     for ref path in env::args().skip(1) {
