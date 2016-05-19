76ca6d63ca92c95ef662b1811213fc0a855894bf
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2016-05-19 22:20:47 +0300

    Add man page to pwd, -h and --help options.

--- src/bin/pwd.rs
+++ src/bin/pwd.rs
@@ -7,10 +7,36 @@ use std::io::{stdout, stderr, Write};
 
 use extra::option::OptionalExt;
 
+const MAN_PAGE: &'static str = /* @MANSTART{pwd} */ r#"
+NAME
+    pwd - return working directory name
+
+SYNOPSIS
+    pwd [ -h | --help ]
+
+DESCRIPTION
+    The pwd utility writes the absolute pathname of the current working directory to the standard output.
+
+OPTIONS
+    -h
+    --help
+        Print this manual page.
+"#; /* @MANEND */
+
 fn main() {
     let stdout = stdout();
     let mut stdout = stdout.lock();
     let mut stderr = stderr();
+
+    if env::args().count() == 2 {
+        if let Some(arg) = env::args().nth(1) {
+            if arg == "--help" || arg == "-h" {
+                stdout.write_all(MAN_PAGE.as_bytes()).try(&mut stderr);
+                stdout.flush().try(&mut stderr);
+                return;
+            }
+        }
+    }
 
     let pwd = env::current_dir().try(&mut stderr);
 
