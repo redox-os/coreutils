0be6f083fcb218e2b4f7c74a487942ff7c5ad8cb
Jeremy Soller <jackpot51@gmail.com>
2016-06-02 20:42:12 -0600

    Fixes for du and ls

--- src/bin/du.rs
+++ src/bin/du.rs
@@ -4,8 +4,7 @@ extern crate extra;
 
 use std::env;
 use std::fs;
-use std::io::{stdout, stderr, Write};
-use extra::io::WriteExt;
+use std::io::{stdout, stderr, StdoutLock, Stderr, Write};
 use extra::option::OptionalExt;
 
 const MAN_PAGE: &'static str = /* @MANSTART{du} */ r#"
@@ -19,51 +18,61 @@ DESCRIPTION
     List the name and size of the FILE(s), or the current directory
 "#; /* @MANEND */
 
-fn print_path(path: &str) {
-    let stdout = stdout();
-    let mut stdout = stdout.lock();
-    let mut stderr = stderr();
-    let mut entries = Vec::new();
+fn list_entry(path: &str, name: &str, stdout: &mut StdoutLock, stderr: &mut Stderr) {
+    let metadata = fs::metadata(path).try(stderr);
+    let size = metadata.len();
 
-    let dir = fs::read_dir(path).try(&mut stderr);
+    stdout.write(((size + 1023) / 1024).to_string().as_bytes()).try(stderr);
+    stdout.write(b"\t").try(stderr);
+    stdout.write(name.as_bytes()).try(stderr);
+    stdout.write(b"\n").try(stderr);
+}
 
-    for entry_result in dir {
-        let entry = entry_result.try(&mut stderr);
+fn list_dir(path: &str, stdout: &mut StdoutLock, stderr: &mut Stderr) {
+    if fs::metadata(path).try(stderr).is_dir() {
+        let dir = fs::read_dir(path).try(stderr);
 
-        let file_type = entry.file_type().try(&mut stderr);
-        let directory = file_type.is_dir();
+        let mut entries = Vec::new();
+        for entry_result in dir {
+            let entry = entry_result.try(stderr);
+            let directory = entry.file_type().map(|x| x.is_dir()).unwrap_or(false);
 
-        if let Some(path_str) = entry.file_name().to_str() {
+            let file_name = entry.file_name();
+            let path_str = file_name.to_str().try(stderr);
             entries.push(path_str.to_owned());
+
             if directory {
                 entries.last_mut().unwrap().push('/');
             }
-        } else {
-            stderr.writeln(b"warning: failed to convert path to a valid string.").try(&mut stderr);
         }
-    }
 
-    entries.sort();
+        entries.sort();
 
-    for entry in entries.iter() {
-        let mut entry_path = path.to_owned();
-        if !entry_path.ends_with('/') {
-            entry_path.push('/');
+        for entry in entries {
+            let mut entry_path = path.to_owned();
+            if !entry_path.ends_with('/') {
+                entry_path.push('/');
+            }
+            entry_path.push_str(&entry);
+
+            list_entry(&entry_path, &entry, stdout, stderr);
         }
-        entry_path.push_str(entry);
-
-        let metadata = fs::metadata(&entry_path).try(&mut stderr);
-        let size = metadata.len();
-
-        stdout.write(((size + 1023) / 1024).to_string().as_bytes()).try(&mut stderr);
-        stdout.write(b"\t").try(&mut stderr);
-        stdout.writeln(entry.as_bytes()).try(&mut stderr);
+    } else {
+        list_entry(path, path, stdout, stderr);
     }
 }
 fn main() {
-    if let Some(ref x) = env::args().nth(1) {
-        print_path(x);
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    let mut stderr = stderr();
+
+    let mut args = env::args().skip(1);
+    if let Some(ref x) = args.next() {
+        list_dir(x, &mut stdout, &mut stderr);
+        for y in args {
+            list_dir(&y, &mut stdout, &mut stderr);
+        }
     } else {
-        print_path(".");
+        list_dir(".", &mut stdout, &mut stderr);
     }
 }
--- src/bin/ls.rs
+++ src/bin/ls.rs
@@ -4,7 +4,7 @@ extern crate extra;
 
 use std::env;
 use std::fs;
-use std::io::{stdout, stderr, Write};
+use std::io::{stdout, stderr, StdoutLock, Stderr, Write};
 use extra::option::OptionalExt;
 
 const MAN_PAGE: &'static str = /* @MANSTART{ls} */ r#"
@@ -18,40 +18,50 @@ DESCRIPTION
     List information about the FILE(s), or the current directory
 "#; /* @MANEND */
 
-fn print_path(path: &str) {
-    let mut entries = Vec::new();
+fn list_entry(name: &str, stdout: &mut StdoutLock, stderr: &mut Stderr) {
+    stdout.write(name.as_bytes()).try(stderr);
+    stdout.write(b"\n").try(stderr);
+}
 
+fn list_dir(path: &str, stdout: &mut StdoutLock, stderr: &mut Stderr) {
+    if fs::metadata(path).try(stderr).is_dir() {
+        let dir = fs::read_dir(path).try(stderr);
+
+        let mut entries = Vec::new();
+        for entry_result in dir {
+            let entry = entry_result.try(stderr);
+            let directory = entry.file_type().map(|x| x.is_dir()).unwrap_or(false);
+
+            let file_name = entry.file_name();
+            let path_str = file_name.to_str().try(stderr);
+            entries.push(path_str.to_owned());
+
+            if directory {
+                entries.last_mut().unwrap().push('/');
+            }
+        }
+
+        entries.sort();
+
+        for entry in entries {
+            list_entry(&entry, stdout, stderr);
+        }
+    } else {
+        list_entry(path, stdout, stderr);
+    }
+}
+fn main() {
     let stdout = stdout();
     let mut stdout = stdout.lock();
     let mut stderr = stderr();
 
-    let dir = fs::read_dir(path).try(&mut stderr);
-
-    for entry_result in dir {
-        let entry = entry_result.try(&mut stderr);
-        let directory = entry.file_type().map(|x| x.is_dir()).unwrap_or(false);
-
-        let file_name = entry.file_name();
-        let path_str = file_name.to_str().try(&mut stderr);
-        entries.push(path_str.to_owned());
-
-        if directory {
-            entries.last_mut().unwrap().push('/');
+    let mut args = env::args().skip(1);
+    if let Some(ref x) = args.next() {
+        list_dir(x, &mut stdout, &mut stderr);
+        for y in args {
+            list_dir(&y, &mut stdout, &mut stderr);
         }
-    }
-
-    entries.sort();
-
-    for entry in entries {
-        stdout.write(entry.as_bytes()).try(&mut stderr);
-        stdout.write(b"\n").try(&mut stderr);
-    }
-}
-
-fn main() {
-    if let Some(ref x) = env::args().nth(1) {
-        print_path(x);
     } else {
-        print_path(".");
+        list_dir(".", &mut stdout, &mut stderr);
     }
 }
