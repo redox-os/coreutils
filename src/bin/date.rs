a1ed89f21dd7ffec2eecf2b6801f0ece415af7f4
Jeremy Soller <jackpot51@gmail.com>
2016-10-08 18:45:52 -0600

    Format times (#82)

--- src/bin/date.rs
+++ src/bin/date.rs
@@ -24,21 +24,58 @@ OPTIONS
         display this help and exit
 "#; /* @MANEND */
 
+// Sweet algorithm from http://ptspts.blogspot.com/2009/11/how-to-convert-unix-timestamp-to-civil.html
+// TODO: Apply timezone offset
+fn format_time(mut ts: i64) -> String {
+    let s = ts%86400;
+    ts /= 86400;
+    let h = s/3600;
+    let m = s/60%60;
+    let s = s%60;
+    let x = (ts*4+102032)/146097+15;
+    let b = ts+2442113+x-(x/4);
+    let mut c = (b*20-2442)/7305;
+    let d = b-365*c-c/4;
+    let mut e = d*1000/30601;
+    let f = d-e*30-e*601/1000;
+    if e < 14 {
+        c -= 4716;
+        e -= 1;
+    } else {
+        c -= 4715;
+        e -= 13;
+    }
+    format!("{}-{}-{} {}:{}:{}", c, e, f, h, m, s)
+}
+
 fn main() {
     let stdout = stdout();
     let mut stdout = stdout.lock();
     let mut stderr = stderr();
 
+    let mut tz_offset = 0;
     for arg in env::args().skip(1){
         if arg.as_str() == "-h" || arg.as_str() == "--help" {
             stdout.write(MAN_PAGE.as_bytes()).try(&mut stderr);
             stdout.flush().try(&mut stderr);
             exit(0);
+        } else if let Ok(offset) = arg.parse::<i64>() {
+            tz_offset = offset;
         }
     }
 
+    let tz_name = if tz_offset == 0 {
+        format!("UTC")
+    } else if tz_offset > 0 {
+        format!("UTC+{:>02}", tz_offset)
+    } else {
+        format!("UTC-{:>02}", -tz_offset)
+    };
+
     let time = SystemTime::now();
     let duration = time.duration_since(UNIX_EPOCH).try(&mut stderr);
+    let mut ts = duration.as_secs() as i64;
+    ts += tz_offset * 3600;
 
-    stdout.write(&format!("{}\n", duration.as_secs()).as_bytes()).try(&mut stderr);
+    stdout.write(format!("{} {}\n", format_time(ts), tz_name).as_bytes()).try(&mut stderr);
 }
