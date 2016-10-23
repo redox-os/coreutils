941f27fc211581a782d0c3899ce2a66d28648f34
stratact <stratact1@gmail.com>
2016-10-22 21:55:01 -0700

    Adapt cp.rs to ArgParser and fix behavior to POSIX specification

--- src/bin/cp.rs
+++ src/bin/cp.rs
@@ -1,10 +1,14 @@
 #![deny(warnings)]
 
+extern crate coreutils;
 extern crate extra;
 
 use std::env;
 use std::fs;
-use std::io::{self, Seek, SeekFrom, stderr, stdout, Write};
+use std::io::{self, stderr, stdout, Write};
+use std::path;
+use std::process::exit;
+use coreutils::{ArgParser, Flag};
 use extra::io::fail;
 use extra::option::OptionalExt;
 
@@ -13,11 +17,11 @@ NAME
     cp - copy files
 
 SYNOPSIS
-    cp SOURCE_FILE DESTINATION_FILES...
+    cp SOURCE_FILE(S) DESTINATION_FILE...
 
 DESCRIPTION
-    The cp utility copies the contents of the SOURCE_FILE to the DESTINATION_FILES. If multiple
-    destination files are specified, then multiple copies of SOURCE_FILE are created.
+    The cp utility copies the contents of the SOURCE_FILE to the DESTINATION_FILE. If multiple
+    source files are specified, then they are copied to DESTINATION_FILE.
 
 OPTIONS
     -h
@@ -29,29 +33,42 @@ fn main() {
     let stdout = stdout();
     let mut stdout = stdout.lock();
     let mut stderr = stderr();
+    let mut parser = ArgParser::new(1)
+        .add_flag("h", "help");
+    parser.initialize(env::args());
 
-    if env::args().count() == 2 {
-        if let Some(arg) = env::args().nth(1) {
-            if arg == "--help" || arg == "-h" {
-                stdout.write_all(MAN_PAGE.as_bytes()).try(&mut stderr);
-                stdout.flush().try(&mut stderr);
-                return;
-            }
-        }
+    if parser.enabled_flag(Flag::Long("help")) {
+        stdout.write_all(MAN_PAGE.as_bytes()).try(&mut stderr);
+        stdout.flush().try(&mut stderr);
+        exit(0);
     }
 
-    let ref src = env::args().nth(1).fail("No source argument. Use --help to see the usage.", &mut stderr);
-    let dsts = env::args().skip(2);
-
-    if dsts.len() < 1 {
+    if parser.args.is_empty() {
+        fail("No source argument. Use --help to see the usage.", &mut stderr);
+    }
+    else if parser.args.len() == 1 {
         fail("No destination arguments. Use --help to see the usage.", &mut stderr);
     }
-
-    let mut src_file = fs::File::open(src).try(&mut stderr);
-    for ref dst in dsts {
-        let mut dst_file = fs::File::create(dst).try(&mut stderr);
-
-        src_file.seek(SeekFrom::Start(0)).try(&mut stderr);
+    else if parser.args.len() == 2 {
+        let mut src_file = fs::File::create(&parser.args[0]).try(&mut stderr);
+        let mut dst_file = fs::File::create(&parser.args[1]).try(&mut stderr);
         io::copy(&mut src_file, &mut dst_file).try(&mut stderr);
+    }
+    else {
+        // This unwrap won't panic since it's been verified not to be empty
+        let dst = parser.args.pop().unwrap();
+        let dst = path::Path::new(&dst);
+        if dst.is_dir() {
+            for ref arg in parser.args {
+                let src = path::Path::new(arg);
+                fs::copy(src, dst).try(&mut stderr);
+            }
+        }
+        else if dst.is_file() {
+            fail("Destination should be a path, not a file. Use --help to see the usage.", &mut stderr);
+        }
+        else {
+            fail("No destination found. Use --help to see the usage.", &mut stderr);
+        }
     }
 }
