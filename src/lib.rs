e0989e3b75522854e80fbbafbdca2f18c383f1ec
stratact <stratact1@gmail.com>
2016-10-29 17:31:14 -0700

    Simplify `ArgParser` API

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,12 @@ pub enum Param {
     Long(String),
 }
 
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub enum Value {
+    Flag(bool),
+    Opt(Option<String>),
+}
+
 pub trait IntoParam {
     fn into_param(self) -> Param;
 }
@@ -25,17 +31,15 @@ use std::collections::HashMap;
 /// Our homebrewed Arg Parser
 #[derive(Clone, Debug, Default)]
 pub struct ArgParser {
-    flags: HashMap<Param, bool>,
-    opts:  HashMap<Param, Option<String>>,
+    params: HashMap<Param, Value>,
     pub args: Vec<String>,
 }
 
 impl ArgParser {
     /// Create a new ArgParser object
-    pub fn new(flag_cap: usize, opt_cap: usize) -> Self {
+    pub fn new(capacity: usize) -> Self {
         ArgParser {
-            flags: HashMap::with_capacity(flag_cap),
-            opts: HashMap::with_capacity(opt_cap),
+            params: HashMap::with_capacity(capacity),
             args: Vec::new(),
         }
     }
@@ -43,10 +47,10 @@ impl ArgParser {
     /// Builder method for adding both short and long flags
     pub fn add_flag(mut self, short: &str, long: &str) -> Self {
         if let Some(short) = short.chars().next() {
-            self.flags.insert(Param::Short(short), false);
+            self.params.insert(Param::Short(short), Value::Flag(false));
         }
         if !long.is_empty() {
-            self.flags.insert(Param::Long(long.to_owned()), false);
+            self.params.insert(Param::Long(long.to_owned()), Value::Flag(false));
         }
         self
     }
@@ -54,17 +58,12 @@ impl ArgParser {
     /// Builder method for adding both short and long opts
     pub fn add_opt(mut self, short: &str, long: &str) -> Self {
         if let Some(short) = short.chars().next() {
-            self.opts.insert(Param::Short(short), None);
+            self.params.insert(Param::Short(short), Value::Opt(None));
         }
         if !long.is_empty() {
-            self.opts.insert(Param::Long(long.to_owned()), None);
+            self.params.insert(Param::Long(long.to_owned()), Value::Opt(None));
         }
         self
-    }
-
-    /// Check if a flag has been used
-    pub fn enabled_flag<F: IntoParam>(&self, flag: F) -> bool {
-        *self.flags.get(&flag.into_param()).unwrap_or(&false)
     }
 
     /// Start parsing user inputted args for which flags are used
@@ -77,23 +76,22 @@ impl ArgParser {
                 arg.remove(0);
                 if let Some(i) = arg.find('=') {
                     let (lhs, rhs) = arg.split_at(i);
-                    if let Some(opt) = self.opts.get_mut(&Param::Long(lhs.to_owned())) {
-                        *opt = Some(rhs.to_owned());
+                    if let Some(&mut Value::Opt(Some(ref mut value))) = self.params.get_mut(&Param::Long(lhs.to_owned())) {
+                        *value = rhs.to_owned();
                     }
                 }
                 else {
-                    if let Some(flag) = self.flags.get_mut(&Param::Long(arg)) {
-                        *flag = true;
+                    if let Some(&mut Value::Flag(ref mut switch)) = self.params.get_mut(&Param::Long(arg)) {
+                        *switch = true;
                     }
                 }
             }
             else if arg.starts_with("-") {
                 for ch in arg[1..].chars() {
-                    if let Some(switch) = self.flags.get_mut(&Param::Short(ch)) {
-                        *switch = true;
-                    }
-                    if let Some(value) = self.opts.get_mut(&Param::Short(ch)) {
-                        *value = args.next();
+                    match self.params.get_mut(&Param::Short(ch)) {
+                        Some(&mut Value::Flag(ref mut switch)) => *switch = true,
+                        Some(&mut Value::Opt(ref mut value)) => *value = args.next(),
+                        None => (),
                     }
                 }
             }
@@ -103,23 +101,25 @@ impl ArgParser {
         }
     }
 
+    /// Check if a flag has been used
+    pub fn flagged<P: IntoParam>(&self, name: P) -> bool {
+        match self.params.get(&name.into_param()) {
+            Some(&Value::Flag(switch)) => switch,
+            Some(&Value::Opt(Some(_))) => true,
+            _ => false,
+        }
+    }
+
     pub fn set_flag<F: IntoParam>(&mut self, flag: F, state: bool) {
-        if let Some(switch) = self.flags.get_mut(&flag.into_param()) {
+        if let Some(&mut Value::Flag(ref mut switch)) = self.params.get_mut(&flag.into_param()) {
             *switch = state;
         }
     }
 
     pub fn set_opt<O: IntoParam>(&mut self, opt: O, state: Option<String>) {
-        if let Some(value) = self.opts.get_mut(&opt.into_param()) {
+        if let Some(&mut Value::Opt(ref mut value)) = self.params.get_mut(&opt.into_param()) {
             *value = state;
         }
-    }
-
-    pub fn has_opt<O: IntoParam>(&mut self, opt: O) -> bool {
-        if let Some(&Some(_)) = self.opts.get(&opt.into_param()) {
-            return true;
-        }
-        false
     }
 }
 
